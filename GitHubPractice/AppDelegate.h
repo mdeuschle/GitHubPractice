3336f16cfc1036873890d748da934417250edca0
MakerMac <mac@mobilemakers.co>
2015-10-02 15:36:02 -0500

    Initial Commit

---
+++ GitHubPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubPractice
+//
+//  Created by MM Driver on 10/2/15.
+//  Copyright © 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubPractice
+//
+//  Created by MM Driver on 10/2/15.
+//  Copyright © 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
